bb82f6ca10bd90fadecfa835e15b6b053eca3ada
Brian <brian@brianvilloldo.com>
2026-09-12 18:31:44 -0300

    feat: add visual home preview to Sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,6 +6,7 @@
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
+import {presentationTool} from 'sanity/presentation'
 import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
@@ -21,6 +22,11 @@ export default defineConfig({
   schema,
   plugins: [
     structureTool({structure}),
+    presentationTool({
+      title: 'Ver Home',
+      previewUrl: 'https://premios-idolos.vercel.app',
+      allowOrigins: ['https://premios-idolos.vercel.app'],
+    }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
